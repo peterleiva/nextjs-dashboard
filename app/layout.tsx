d6e2c8d685ee7c7d6ade8f41baaaddf6273a0360
Peter <contact@peter.dev.br>
2024-01-07 14:42:09 -0300

    Add optmized fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import clsx from 'clsx';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
   );
+  `~`;
 }
